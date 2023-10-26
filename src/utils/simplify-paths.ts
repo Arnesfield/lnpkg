565c8278d4e6ce4b2aff6fe9aff1cd5b415f7e16
Jefferson Rylee <rylee.jeff385@gmail.com>
2023-10-26 12:07:53 +0800

    Fix simplifyPaths

--- src/utils/simplify-paths.ts
+++ src/utils/simplify-paths.ts
@@ -15,8 +15,9 @@ export function simplifyPaths(paths: string[]): string[] {
   const simplifiedPaths = paths.sort().splice(0, 1);
   let previous = simplifiedPaths[0];
   for (const value of paths) {
-    const a = path.dirname(value) + path.sep;
-    const b = previous + path.sep;
+    let a = path.dirname(value);
+    a += a.endsWith(path.sep) ? '' : path.sep;
+    const b = previous + (previous.endsWith(path.sep) ? '' : path.sep);
     if (!a.startsWith(b)) {
       previous = value;
       simplifiedPaths.push(value);
