c347376b56f20b66c04dd5eca741d6c95db38c71
Jefferson Rylee <rylee.jeff385@gmail.com>
2023-10-27 22:28:01 +0800

    Handle mapped dirs

--- src/cli.ts
+++ src/cli.ts
@@ -13,7 +13,12 @@ function createProgram() {
   return new Command()
     .name(name)
     .description(description)
-    .argument('[dirs...]', 'paths to local Node.js packages to link')
+    .argument(
+      '[dirs...]',
+      'paths to local Node.js packages to link.\n' +
+        'Separate with a colon to map specific packages:\n\n' +
+        'lnpkg <src1> : <dest1> <src2> <src3> : <dest3> ...'
+    )
     .option('-t, --target <target>', 'target Node.js package to link', '.')
     .option('-w, --watch', 'watch package files for changes')
     .option(
--- src/helpers/get-entries.ts
+++ src/helpers/get-entries.ts
@@ -1,45 +1,44 @@
 import path from 'path';
 import { Entry, LnPkgOptions } from '../types/core.types';
 
+function add(entries: Entry[], entry: Entry) {
+  entry.src = path.resolve(entry.src);
+  entry.dest = path.resolve(entry.dest);
+  // check if path map already exists
+  const exists = entries.some(
+    existing => existing.src === entry.src && existing.dest === entry.dest
+  );
+  if (!exists) {
+    entries.push(entry);
+  }
+}
+
 export function getEntries(options: LnPkgOptions): Entry[] {
   const { paths, target } = options;
   if (paths.length === 0) {
     throw new Error('No paths specified.');
   }
-
+  const sep = ':';
   const entries: Entry[] = [];
-  function add(entry: Entry) {
-    entry.src = path.resolve(entry.src);
-    entry.dest = path.resolve(entry.dest);
-    // check if path map already exists
-    const exists = entries.some(
-      existing => existing.src === entry.src && existing.dest === entry.dest
-    );
-    if (!exists) {
-      entries.push(entry);
+  for (let index = 0; index < paths.length; index++) {
+    const src = paths[index];
+    if (typeof src === 'object') {
+      add(entries, src);
+      continue;
+    } else if (src === sep || src.startsWith('-')) {
+      continue;
     }
-  }
-
-  for (const value of paths) {
-    if (typeof value === 'string') {
+    const next = paths[index + 1];
+    const dest = paths[index + 2];
+    if (next === sep && dest && typeof dest === 'string') {
+      add(entries, { src, dest });
+      index += 2;
+    } else if (!target || typeof target !== 'string') {
       // check target
-      if (!target || typeof target !== 'string') {
-        throw new Error('Missing "target" path option.');
-      }
-      add({ src: value, dest: target });
-    } else if (
-      value &&
-      typeof value === 'object' &&
-      value.src &&
-      typeof value.src === 'string' &&
-      value.dest &&
-      typeof value.dest === 'string'
-    ) {
-      add(value);
+      throw new Error('Missing "target" path option.');
     } else {
-      throw new Error(`Not a valid entry: ${value}`);
+      add(entries, { src, dest: target });
     }
   }
-
   return entries;
 }
