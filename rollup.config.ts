3d8485875e424f8806d80353c3aee8de3d109d9e
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-03-25 20:12:59 +0800

    Use cleanup plugin and remove sourcemaps from build

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import { createRequire } from 'module';
 import { RollupOptions } from 'rollup';
+import cleanup from 'rollup-plugin-cleanup';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import externals from 'rollup-plugin-node-externals';
@@ -12,7 +13,6 @@ import type Pkg from './package.json';
 const require = createRequire(import.meta.url);
 const pkg: typeof Pkg = require('./package.json');
 const input = 'src/index.ts';
-// skip sourcemap and umd unless production
 const WATCH = process.env.ROLLUP_WATCH === 'true';
 const PROD = !WATCH || process.env.NODE_ENV === 'production';
 
@@ -27,12 +27,20 @@ export default defineConfig([
       dir: 'lib',
       format: 'esm',
       exports: 'named',
-      sourcemap: PROD,
       chunkFileNames: '[name].js'
     },
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    plugins: [esbuild({ target: 'esnext' }), json(), externals(), outputSize()]
+    plugins: [
+      esbuild({ target: 'esnext' }),
+      cleanup({
+        comments: ['some', 'sources', /__PURE__/],
+        extensions: ['js', 'ts']
+      }),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      json(),
+      externals(),
+      outputSize()
+    ]
   },
   {
     input,
