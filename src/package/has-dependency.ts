08492260278782b8c1de22ad5b51eb896bf8b16e
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-04-11 13:26:39 +0800

    Update hasDependency check to use hasOwnProperty

--- src/package/has-dependency.ts
+++ src/package/has-dependency.ts
@@ -1,17 +1,16 @@
 import { PackageJson } from '@npmcli/package-json';
 
 export function hasDependency(json: PackageJson, name: string): boolean {
-  // NOTE: expect that __proto__ is never used anyway
-  // bypass check with force flag
-  return !!(
-    name !== '__proto__' &&
-    (json.dependencies?.[name] ||
-      json.devDependencies?.[name] ||
-      json.peerDependencies?.[name] ||
-      json.optionalDependencies?.[name] ||
-      (Array.isArray(json.bundleDependencies) &&
-        json.bundleDependencies.includes(name)) ||
-      (Array.isArray(json.bundledDependencies) &&
-        json.bundledDependencies.includes(name)))
+  return (
+    [
+      json.dependencies,
+      json.devDependencies,
+      json.peerDependencies,
+      json.optionalDependencies
+    ].some(deps => deps && Object.prototype.hasOwnProperty.call(deps, name)) ||
+    (Array.isArray(json.bundleDependencies) &&
+      json.bundleDependencies.includes(name)) ||
+    (Array.isArray(json.bundledDependencies) &&
+      json.bundledDependencies.includes(name))
   );
 }
